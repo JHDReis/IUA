5d2f0df231bebdb784357627eb36dd5f1f33f0ac
JoaoReis <jjr.reis@gmail.com>
2025-11-28 12:16:25 +0100

    save work in progress

--- lib/include/Utility.h
+++ lib/include/Utility.h
@@ -1,5 +1,46 @@
 #pragma once
 
+#include <map>
+#include <new>
 #include <string>
 
 std::string getVersion();
+
+template<typename ITEM> ITEM* rawMemory(const int n)
+{
+    return static_cast<ITEM*>(::operator new(sizeof(ITEM) * n));
+}
+
+inline void rawDelete(void* array)
+{
+    ::operator delete(array);
+}
+
+template<typename ITEM> void rawDestructor(ITEM* array, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        array[i].~ITEM();
+    }
+    rawDelete(array);
+}
+
+//  Placement New.
+// In C++, this specific syntax is used to construct an object at a specific memory address
+// that has already been allocated,// rather than allocating fresh memory from the heap.
+template<typename TYPE> TYPE& genericAssign(TYPE& to, TYPE const& rhs)
+{
+    if (&to != &rhs) {
+        to.~TYPE();
+        new (&to) TYPE(rhs);
+    }
+    return to;
+}
+
+template<typename KEY, typename VALUE> struct KVPair {
+    KEY key;
+    VALUE value;
+    explicit KVPair(const KEY& k = KEY(), const VALUE& v = VALUE())
+        : key(k), value(v)
+    {}
+};
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,34 @@
-#include <catch2\catch_all.hpp>
 #include "Utility.h"
-
+#include <catch2/catch_all.hpp>
 #include <iostream>
-
 
 TEST_CASE("Basic test", "[main]")
 {
     REQUIRE(getVersion() == "0.1");
+}
+
+class User {
+public:
+    int id;
+    std::string name;
+
+    User(int id, const std::string& name): id(id), name(name)
+    {
+        std::cout << "Constructing User: " << name << "\n";
+    }
+
+    ~User() { std::cout << "Destroying User: " << name << "\n"; }
+
+    User& operator=(const User& rhs) { return genericAssign(*this, rhs); }
+};
+
+TEST_CASE("Generic Assign", "[main]")
+{
+
+    User a(1, "A");
+    User b(2, "B");
+
+    a = b;
+
+    CHECK(b.id == a.id);
 }
