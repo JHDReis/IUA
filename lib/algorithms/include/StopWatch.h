102eee1ebe9a485041942f39b89a31ee5f176462
JoaoReis <jjr.reis@gmail.com>
2025-12-15 17:40:18 +0100

    Add spdlog as submodule

---
+++ lib/algorithms/include/StopWatch.h
@@ -0,0 +1,10 @@
+//
+// Created by joaoreis on 2025 Dec 15.
+//
+
+#ifndef IUA_STOPWATCH_H
+#define IUA_STOPWATCH_H
+
+class StopWatch {};
+
+#endif // IUA_STOPWATCH_H
---
+++ lib/algorithms/src/StopWatch.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joaoreis on 2025 Dec 15.
+//
+
+#include "../include/StopWatch.h"
