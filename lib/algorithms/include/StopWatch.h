5329c2d04397b30e4267cec976baffde1d9cbe0c
JoaoReis <jjr.reis@gmail.com>
2025-12-15 22:44:20 +0100

    added StopWatch implementation to measure execution time

--- lib/algorithms/include/StopWatch.h
+++ lib/algorithms/include/StopWatch.h
@@ -1,10 +1,24 @@
-//
-// Created by joaoreis on 2025 Dec 15.
-//
 
 #ifndef IUA_STOPWATCH_H
 #define IUA_STOPWATCH_H
+#include <memory>
+#include <string>
 
-class StopWatch {};
+class StopWatch {
+    class Impl;
+    std::unique_ptr<Impl> _impl;
+
+public:
+    explicit StopWatch(const std::string& message);
+
+    ~StopWatch();
+
+    // do not allow copy or move
+    StopWatch(const StopWatch& other) = delete;
+    StopWatch(StopWatch&& other) noexcept = delete;
+
+    StopWatch& operator=(const StopWatch& other) = delete;
+    StopWatch& operator=(StopWatch&& other) noexcept = delete;
+};
 
 #endif // IUA_STOPWATCH_H
--- lib/algorithms/src/StopWatch.cpp
+++ lib/algorithms/src/StopWatch.cpp
@@ -1,5 +1,34 @@
-//
-// Created by joaoreis on 2025 Dec 15.
-//
 
-#include "../include/StopWatch.h"
+#include "StopWatch.h"
+#include "spdlog/spdlog.h"
+#include <spdlog/fmt/chrono.h>
+#include "spdlog/stopwatch.h"
+#include <utility>
+
+#if defined(__linux__)               // Linux
+    #define FUNCTION_NAME __PRETTY_FUNCTION__
+#else
+    #define FUNCTION_NAME __func__      // Fallback to __func__ if OS is not Linux
+#endif
+
+using namespace std::chrono;
+
+class StopWatch::Impl : spdlog::stopwatch {
+public:
+    explicit Impl(std::string message): _message(std::move(message)) {}
+
+    ~Impl()
+    {
+        const auto ms = duration_cast<milliseconds>(this->elapsed());
+        spdlog::info("[{}] : {} took {} ms", FUNCTION_NAME, _message, ms);
+    }
+
+private:
+    std::string _message{};
+};
+
+StopWatch::StopWatch(const std::string& message)
+    : _impl(std::make_unique<Impl>(message))
+{}
+
+StopWatch::~StopWatch() = default;
